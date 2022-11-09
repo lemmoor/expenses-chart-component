6677901e5fa3686c02f0939aaf494d6dc3aec8ee
lemmoor <49235295+lemmoor@users.noreply.github.com>
2022-11-09 20:53:43 +0100

    Mobile styles

---
+++ script.js
@@ -0,0 +1,11 @@
+fetch("data.JSON")
+.then(res => res.json())
+.then(data => {
+    const chartBars = document.querySelectorAll(".chart-bar");
+    let amount = data.map(e => e.amount);
+    let max = Math.max(...amount);
+    chartBars.forEach((bar, i) => {
+        bar.style.height = (amount[i] / max) * 100 + "%";
+    })
+})
+
