7ce244d50fd36a043f378cf2f45f5a845fde85a9
lemmoor <49235295+lemmoor@users.noreply.github.com>
2022-11-10 22:14:38 +0100

    can't fetch a json on github pages for some reason

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
-fetch("./data.JSON")
-.then(res => res.json())
-.then(data => {
-    const chartBars = document.querySelectorAll(".chart-bar");
-    let amount = data.map(e => e.amount);
-    let max = Math.max(...amount);
-    chartBars.forEach((bar, i) => {
-        bar.style.height = (amount[i] / max) * 100 + "%";
-        bar.dataset.amount = "$" + amount[i];
-    })
-})
+// fetch("./data.JSON")
+// .then(res => res.json())
+// .then(data => {
+const data = [
+    {
+      "day": "mon",
+      "amount": 17.45
+    },
+    {
+      "day": "tue",
+      "amount": 34.91
+    },
+    {
+      "day": "wed",
+      "amount": 52.36
+    },
+    {
+      "day": "thu",
+      "amount": 31.07
+    },
+    {
+      "day": "fri",
+      "amount": 23.39
+    },
+    {
+      "day": "sat",
+      "amount": 43.28
+    },
+    {
+      "day": "sun",
+      "amount": 25.48
+    }
+  ]
+
+const chartBars = document.querySelectorAll(".chart-bar");
+let amount = data.map(e => e.amount);
+let max = Math.max(...amount);
+chartBars.forEach((bar, i) => {
+    bar.style.height = (amount[i] / max) * 100 + "%";
+    bar.dataset.amount = "$" + amount[i];
+    });
+// })
 
